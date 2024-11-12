a21901e59327dced02f0674b70803565aba2319a
Chong Wei Bing <2719380C@student.gla.ac.uk>
2024-11-12 13:23:39 +0800

    fix createplan backend logic
    - should not have duplicate plan name under the same app due to CK constraints

--- backend/controllers/planController.js
+++ backend/controllers/planController.js
@@ -17,29 +17,37 @@ const createPlan = async (req, res) => {
     return res.json({ success: false, message: "Plan name must be alphanumeric have a maximum of 255 characters" })
   }
 
-  if (!plan_startdate) {
-    return res.json({ success: false, message: "Plan start date is mandatory" })
-  }
-
-  if (!plan_enddate) {
-    return res.json({ success: false, message: "Plan end date is mandatory" })
-  }
-
-  // plan start date must be before app end date
-  if (plan_startdate >= plan_enddate) {
-    return res.json({ success: false, message: "App start date must be before app end date" })
-  }
-
-  if (!plan_color) {
-    return res.json({ success: false, message: "Plan color is mandatory" })
-  }
-
+  // check if plan name already exists under this app
   const connection = await pool.getConnection()
 
   try {
-    const query = `INSERT INTO plan(plan_mvp_name, plan_app_acronym, plan_startdate, plan_enddate, plan_color) VALUES (?, ?, ?, ?, ?)`
-    await connection.query(query, [plan_mvp_name, plan_app_acronym, plan_startdate, plan_enddate, plan_color])
-    return res.json({ success: true, message: "Plan created" })
+    const query = `SELECT * FROM plan WHERE plan_mvp_name = ? AND plan_app_acronym = ?`
+    const [results] = await connection.query(query, [plan_mvp_name, plan_app_acronym])
+
+    if (results.length > 0) {
+      return res.json({ success: false, message: "Plan name already exists" })
+    } else {
+      if (!plan_startdate) {
+        return res.json({ success: false, message: "Plan start date is mandatory" })
+      }
+
+      if (!plan_enddate) {
+        return res.json({ success: false, message: "Plan end date is mandatory" })
+      }
+
+      // plan start date must be before app end date
+      if (plan_startdate >= plan_enddate) {
+        return res.json({ success: false, message: "App start date must be before app end date" })
+      }
+
+      if (!plan_color) {
+        return res.json({ success: false, message: "Plan color is mandatory" })
+      }
+
+      const insertQuery = `INSERT INTO plan(plan_mvp_name, plan_app_acronym, plan_startdate, plan_enddate, plan_color) VALUES (?, ?, ?, ?, ?)`
+      await connection.query(insertQuery, [plan_mvp_name, plan_app_acronym, plan_startdate, plan_enddate, plan_color])
+      return res.json({ success: true, message: "Plan created" })
+    }
   } catch (err) {
     return res.status(500).json({ success: false, message: "Unable to create plan", stack: err.stack })
   } finally {
