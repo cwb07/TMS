efc8b99efb4ed270238cd08bdd37d1de05b3c45d
Chong Wei Bing <2719380C@student.gla.ac.uk>
2024-11-28 17:09:48 +0800

    update

---
+++ dev-forbuild/server.js
@@ -0,0 +1,25 @@
+import MsgCode from "./constants.js"
+import dotenv from "dotenv"
+import express from "express"
+import routes from "./routes.js"
+
+dotenv.config()
+const PORT = process.env.PORT
+
+const app = express()
+
+app.use(express.json())
+
+app.use((err, req, res, next) => {
+    if (err instanceof SyntaxError) {
+        res.json({ MsgCode: MsgCode.PAYLOAD_SYNTAX_ERROR })
+    }
+});
+
+app.use(routes)
+
+app.use((req, res) => {
+    res.json({ MsgCode: MsgCode.INVALID_URL })
+})
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
